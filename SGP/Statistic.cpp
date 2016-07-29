150ffece6d7483ea16b0e0fc270af073a5b1a1eb
miner2006 <miner2006@839dda39-949c-4daa-b7f8-d55851b276c3>
2016-07-29 10:03:45 +0000

    forget to add new files on the previous commit:(

---
+++ SGP/Statistic.cpp
@@ -0,0 +1,243 @@
+#include "StdAfx.h"
+#include "Statistic.h"
+#include "Log.h"
+#include <sstream>
+
+Statistic::Statistic(int k)
+{
+	_k = k;
+	_repartition_count=0;
+	_sample_hit = 0;
+
+	for(int i=0; i<_k; i++)
+	{
+		PartitionStatisticInfo info;
+		info._assign_external_links = 0;
+		info._assign_internal_links = 0;
+		info._assign_vex_number = 0;
+		info._partition_external_links = 0;
+		info._partition_internal_links = 0;
+		info._partition_vex_number = 0;
+		_partitions_statistic.push_back(info);
+	}
+}
+
+
+Statistic::~Statistic(void)
+{
+}
+
+void Statistic::IncreasePartitionInternalLinks(int partition)
+{
+	_partitions_statistic[partition]._partition_internal_links++;
+}
+void Statistic::IncreasePartitionExternalLinks(int partition)
+{
+	_partitions_statistic[partition]._partition_external_links++;
+}
+void Statistic::IncreaseAssignInternalLinks(int partition)
+{
+	_partitions_statistic[partition]._assign_internal_links++;
+}
+void Statistic::IncreaseAssignExternalLinks(int partition)
+{
+	_partitions_statistic[partition]._assign_external_links++;
+}
+void Statistic::IncreaseRepartitionCount()
+{
+	_repartition_count++;
+}
+void Statistic::AppendPartitionExchangeCount(int exchange_count)
+{
+	_partition_exchange_count_array.push_back(exchange_count);
+}
+int Statistic::GetPartitionInternalLinks(int partition)
+{
+	return _partitions_statistic[partition]._partition_internal_links;
+}
+int Statistic::GetPartitionExternalLinks(int partition)
+{
+	return _partitions_statistic[partition]._partition_external_links;
+}	
+int Statistic::GetAssignInternalLinks(int partition)
+{
+	return _partitions_statistic[partition]._assign_internal_links;
+}	
+int Statistic::GetAssignExternalLinks(int partition)
+{
+	return _partitions_statistic[partition]._assign_external_links;
+}	
+int Statistic::GetRepartitionCount()
+{
+	return _repartition_count;
+}	
+int Statistic::GetTotalCutValue()
+{
+	return GetTotalExternalLinks();
+}
+
+int Statistic::GetPartitionCutValue()
+{
+	int sum = 0;
+	for(int i=0; i<_k; i++)
+	{
+		sum += _partitions_statistic[i]._partition_external_links;
+	}
+	return sum/2;
+}
+
+int Statistic::GetTotalInternalLinks()
+{
+	int sum = 0;
+	for(int i=0; i<_k; i++)
+	{
+		sum += _partitions_statistic[i]._assign_internal_links+_partitions_statistic[i]._partition_internal_links;
+	}
+	return sum;
+}	
+int Statistic::GetTotalExternalLinks()
+{
+	int sum = 0;
+	for(int i=0; i<_k; i++)
+	{
+		sum += _partitions_statistic[i]._assign_external_links+_partitions_statistic[i]._partition_external_links;
+	}
+	return sum/2;
+}
+
+void Statistic::SetPartitionVexNumber(int partition, int n)
+{
+	_partitions_statistic[partition]._partition_vex_number = n;
+}
+
+void Statistic::SetAssignVexNumber(int partition, int n)
+{
+	_partitions_statistic[partition]._assign_vex_number = n;
+}
+
+void Statistic::printStatistic()
+{
+	int cutvalue = 0;
+	stringstream str;
+	for(int i=0; i<_k; i++)
+	{
+		str.str("");
+		str	<<"\n\n>>Partition "<<i<<" Statistic \n"
+			<<"Partition Vertex number: \t"<<GetPartitionVexNumber(i)<<"\n"
+			<<"Assigned Vertex number: \t"<<GetAssignVexNumber(i)<<"\n"
+			<<"Partition Internal Edges: \t"<<GetPartitionInternalLinks(i)<<"\n"
+			<<"Partition External Edges: \t"<<GetPartitionExternalLinks(i)<<"\n"
+			<<"Assign Internal Edges: \t"<<GetAssignInternalLinks(i)<<"\n"
+			<<"Assign External Edges: \t"<<GetAssignExternalLinks(i)<<"\n";
+		Log::log(str.str());
+	}
+
+	str.str("");
+	str<<"\n>>In Total \n Total Cut Value: \t"<<GetTotalCutValue()<<"\n"
+		<<"Parititoner Cut Value: \t"<<GetPartitionCutValue()<<"\n"
+		<<"Total Partition Internal Edges: \t"<<GetTotalPartitionInternalLinks()<<"\n"
+		<<"Total Partition External Edges: \t"<<GetTotalPartitionExternalLinks()<<"\n"
+		<<"Total Assign Internal Edges: \t"<<GetTotalAssignInternalLinks()<<"\n"
+		<<"Total Assign External Edges: \t"<<GetTotalAssignExternalLinks()<<"\n"
+		<<"Total Internal Edges: \t"<<GetTotalInternalLinks()<<"\n"
+		<<"Total External Edges: \t"<<GetTotalExternalLinks()<<"\n"
+		<<"Total Edges: \t"<<GetTotalInternalLinks()+GetTotalExternalLinks()<<"\n"
+		<<"Total Partition Vex: \t"<<GetTotalPartitionVexNumber()<<"\n"
+		<<"Total Assign Vex: \t"<<GetTotalAssignVexNumber()<<"\n"
+		<<"Total  Vex: \t"<<GetTotalVexNumber()<<"\n"
+		<<"Repartition Count: \t"<<GetRepartitionCount()<<"\n"
+		<<"Sample Hit Count: \t"<<GetSampleHit()<<"\n"
+		<<"Exchange Count In Each Partition:\n";
+	for(vector<int>::iterator iter = _partition_exchange_count_array.begin();
+		iter!=_partition_exchange_count_array.end();
+		iter++)
+	{
+		str<<*iter<<"\t";
+	}
+	Log::logln(str.str());
+}
+
+void Statistic::ResetPartitionInfos()
+{
+	for(int i=0; i<_k; i++)
+	{
+		_partitions_statistic[i]._assign_external_links = 0;
+		_partitions_statistic[i]._assign_internal_links = 0;
+		_partitions_statistic[i]._assign_vex_number = 0;
+		_partitions_statistic[i]._partition_external_links = 0;
+		_partitions_statistic[i]._partition_internal_links = 0;
+		_partitions_statistic[i]._partition_vex_number = 0;
+	}
+	_repartition_count=0;
+	_sample_hit = 0;
+	_partition_exchange_count_array.clear();
+}
+
+void Statistic::ResetPartitionInfoofAssignLinks()
+{
+	for(int i=0; i<_k; i++)
+	{
+		_partitions_statistic[i]._assign_external_links = 0;
+		_partitions_statistic[i]._assign_internal_links = 0;
+	}
+}
+
+int Statistic::GetTotalAssignVexNumber()
+{
+	int sum = 0;
+	for(int i=0; i<_k; i++)
+	{
+		sum += _partitions_statistic[i]._assign_vex_number;
+	}
+	return sum;
+}
+int Statistic::GetTotalPartitionVexNumber()
+{
+	int sum = 0;
+	for(int i=0; i<_k; i++)
+	{
+		sum += _partitions_statistic[i]._partition_vex_number;
+	}
+	return sum;
+}
+int Statistic::GetTotalVexNumber()
+{
+	return GetTotalPartitionVexNumber()+GetTotalAssignVexNumber();
+}
+
+int Statistic::GetTotalPartitionInternalLinks()
+{
+	int sum = 0;
+	for(int i=0; i<_k; i++)
+	{
+		sum += _partitions_statistic[i]._partition_internal_links;
+	}
+	return sum;
+}
+int Statistic::GetTotalPartitionExternalLinks()
+{
+	int sum = 0;
+	for(int i=0; i<_k; i++)
+	{
+		sum += _partitions_statistic[i]._partition_external_links;
+	}
+	return sum/2;
+}
+int Statistic::GetTotalAssignInternalLinks()
+{
+	int sum = 0;
+	for(int i=0; i<_k; i++)
+	{
+		sum += _partitions_statistic[i]._assign_internal_links;
+	}
+	return sum;
+}
+int Statistic::GetTotalAssignExternalLinks()
+{
+	int sum = 0;
+	for(int i=0; i<_k; i++)
+	{
+		sum += _partitions_statistic[i]._assign_external_links;
+	}
+	return sum/2;
+}
---
+++ SGP/Statistic.h
@@ -0,0 +1,70 @@
+#pragma once
+#include<vector>
+using namespace std;
+
+//statistic information of a partition
+typedef struct _PartitionStatisticInfo {
+	int	_partition_vex_number; //the number of vertex of a partition
+	int _assign_vex_number;//the number of assigned vertex
+	int _partition_external_links; // external links in the partition
+	int _partition_internal_links; // internal links in the partition
+	int _assign_external_links; //external links in assigned partition
+	int _assign_internal_links; //internal links in assigned partition
+} PartitionStatisticInfo;
+
+class Statistic
+{
+public:
+	Statistic(int k);
+	~Statistic(void);
+private:
+	//the statistic informations of partitions. these informations will be filled up at the step of writing
+	vector<PartitionStatisticInfo> _partitions_statistic;
+	int _repartition_count;
+	vector<int> _partition_exchange_count_array;
+	int _k;
+	int _sample_hit;//采样置换次数
+
+public:
+	void IncreasePartitionInternalLinks(int partition);
+	void IncreasePartitionExternalLinks(int partition);
+	void IncreaseAssignInternalLinks(int partition);
+	void IncreaseAssignExternalLinks(int partition);
+	int GetPartitionInternalLinks(int partition);
+	int GetPartitionExternalLinks(int partition);
+	int GetAssignInternalLinks(int partition);
+	int GetAssignExternalLinks(int partition);
+
+	void IncreaseSampleHit() {_sample_hit++;};
+	int GetSampleHit(){return _sample_hit;};
+
+	void IncreaseRepartitionCount();
+	int GetRepartitionCount();
+
+	void AppendPartitionExchangeCount(int exchange_count);
+	
+	int GetTotalCutValue();
+	int GetPartitionCutValue();
+	int GetTotalInternalLinks();
+	int GetTotalExternalLinks();
+	int GetTotalPartitionInternalLinks();
+	int GetTotalPartitionExternalLinks();
+	int GetTotalAssignInternalLinks();
+	int GetTotalAssignExternalLinks();
+
+	void SetPartitionVexNumber(int partition, int n);
+	void SetAssignVexNumber(int partition, int n);
+	void IncreaseAssignVexNumber(int partition){_partitions_statistic[partition]._assign_vex_number++;};
+	int GetPartitionVexNumber(int partition){return _partitions_statistic[partition]._partition_vex_number;};
+	int GetAssignVexNumber(int partition){return _partitions_statistic[partition]._assign_vex_number;};
+	int GetTotalAssignVexNumber();
+	int GetTotalPartitionVexNumber();
+	int GetTotalVexNumber();
+	
+
+	void ResetPartitionInfos();
+	void ResetPartitionInfoofAssignLinks();
+
+	void printStatistic();
+};
+
